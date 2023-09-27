075603ffd66d93e6eff48d4d4a079abdba081b16
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2023-09-27 12:05:52 +0200

    Fetching hourly data.

--- src/weather.py
+++ src/weather.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 class WeatherData:
-    def __init__(self, stations, elements, time, client_id,
+    def __init__(self, stations, elements, time, client_id, steps=1,
                  url='https://frost.met.no/observations/v0.jsonld'):
         """
         Fetch weather data from the Frost API.
@@ -23,26 +23,48 @@ class WeatherData:
         """
         self.stations = stations
         self.elements = elements
-        self.time = time
         self.client_id = client_id
         self.url = url
 
-        self.data = None
-        self.fetch()
+        if steps == 1:
+            self.data = self.fetch(time)
+        else:
+            start_date_str, end_date_str = time.split('/')
 
-    def fetch(self):
-        """Fetch weather data."""
+            start_date = pd.to_datetime(start_date_str)
+            end_date = pd.to_datetime(end_date_str)
+
+            date_range = pd.date_range(start=start_date, end=end_date, freq='100D')
+
+            data = []
+            for i in range(len(date_range) - 1):
+                start = date_range[i].strftime('%Y-%m-%d')
+                end = date_range[i + 1].strftime('%Y-%m-%d')
+
+                date_range_str = f"{start}/{end}"
+                data.append(self.fetch(date_range_str))
+
+            self.data = pd.concat(data)
+
+    def fetch(self, time):
+        """
+        Fetch weather data.
+
+        Returns
+        -------
+        pandas.DataFrame
+        """
         parameters = {
             'sources': ",".join(self.stations),
             'elements': self.elements,
-            'referencetime': self.time,
+            'referencetime': time,
         }
 
         _response = requests.get(self.url, parameters, auth=(self.client_id, ''))
         response = _response.json()
 
-        self.data = pd.json_normalize(response['data'], record_path='observations',
-                                      meta=['sourceId', 'referenceTime'])
+        return pd.json_normalize(response['data'], record_path='observations',
+                                 meta=['sourceId', 'referenceTime'])
 
     def clean(self, remove=None):
         """
