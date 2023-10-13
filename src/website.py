a00a070a3a5ec7910d8bd046f5973df7a0bc4c36
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2023-10-13 08:37:42 +0200

    streamlit.app --> localhost

--- src/website.py
+++ src/website.py
@@ -5,8 +5,8 @@ from helpers.model import Model
 from helpers.plotting import optimal_times, plot_shading
 
 st.set_page_config(
-    page_title= "Electricity app",
-    page_icon= "<3"
+    page_title="Electricity app",
+    page_icon="<3"
 )
 navigation = st.sidebar.radio("Navigation", ["Home", "About"])
 
@@ -26,7 +26,7 @@ if navigation == "Home":
             the grid."""
         )
         st.write(hidden_text)
-    
+
     with st.expander("How to use"):
         st.markdown("**Battery capacity, kWh**")
         st.text("Enter the capacity of your battery")
@@ -38,7 +38,7 @@ if navigation == "Home":
     col1, col2 = st.columns(2)
     batteri_kap = col1.slider('Battery capasity in kWh', value=50, min_value=10, max_value=100)
     export_capacity = col2.slider('Export capasity kWh/h', min_value=10, max_value=100)
-    hours = math.ceil(batteri_kap/export_capacity)
+    hours = math.ceil(batteri_kap / export_capacity)
 
     st.text(f'To empty your battery you must export for'
             f' ~{hours} hours')
@@ -57,7 +57,7 @@ if navigation == "Home":
 
     st.write("**Sell between:**")
     for i, (start, end) in enumerate(sell_times):
-        st.write(f"{i+1}. {start} - {end}")
+        st.write(f"{i + 1}. {start} - {end}")
 
 elif navigation == "About":
     st.title("About the team")
